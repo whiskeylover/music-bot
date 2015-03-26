5323e606d1fcfbbc378f6bdf61c07077cb6b1308
C.J. Bordeleau <=>
2015-03-26 13:39:13 -0400

    Inital Commit.

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    },
+    wiredep: {
+      task: {
+        // Point to the files that should be updated when
+        // you run `grunt wiredep`
+        src: [
+          'index.html',   // .html support...
+        ],
+        options: {
+          // See wiredep's configuration documentation for the options
+          // you may pass:
+
+          // https://github.com/taptapship/wiredep#configuration
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-wiredep');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
---
+++ README.md
@@ -0,0 +1,10 @@
+# Music Generation Bot
+
+Attept at a web app to generate pleasant sounding music on demand.
+
+work in progress
+
+contributors: 
+  Tim Coffey
+  C.J. Bordeleau
+
